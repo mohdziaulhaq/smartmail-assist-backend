a36b799f5b10c3a482b3a3f443ed41ede1aa3cb0
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-05-13 06:43:47 -0500

    project setup

---
+++ src/main/java/com/zia/smartmail/SmartMailAssistApplication.java
@@ -0,0 +1,13 @@
+package com.zia.smartmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMailAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMailAssistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zia/smartmail/SmartMailAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zia.smartmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMailAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
